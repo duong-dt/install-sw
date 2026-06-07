fbdd743b2830555d3b9852da64c59f5b9a61bfab
duong <dotungduong2@gmail.com>
2026-06-07 22:29:35 +0700

    add script for cachyos

---
+++ install-cachyos.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# DIR="$(cd "$(dirname "$0")" && pwd)"
+# source $HOME/.profile
+
+# cd ${TMPDIR:-/tmp}
+
+# Ask for sudo privilege
+sudo -v || exit $?
+
+# keep-alive sudo session
+while true; do sleep 60; kill -0 "$$" || exit; sudo -v; done 2>/dev/null &
+
+# Install packages from Arch repo
+sudo pacman -Syu --noconfirm \
+jq zbar unrar \
+git zsh yakuake \
+android-tools \
+vlc celluloid easytag easyeffects strawberry \
+komikku calibre okular \
+homebank qbittorrent \
+vorta borg syncthing kdiff3 \
+localsend bitwarden proton-vpn-gtk-app \
+onlyoffice-bin zen-browser-bin betterbird-bin \
+mission-center octopi \
+cachyos-gaming-meta lutris steam \
+fcitx5 fcitx5-configtool fcitx5-unikey fcitx5-mozc \
+distrobox toolbox podman \
+fd fzf ripgrep bat micro \
+zoxide uv lazygit just yazi \
+ttf-firacode-nerd \
+curl wget nodejs neovim \
+python-pyfuse3
+
+# Install packages from AUR
+paru -S --noconfirm \
+sourcegit-bin megasync-bin
+
+# Install tools from uv
+uv tool install virtualenvwrapper
+
---
+++ remove-cachyos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#source $HOME/.profile
+
+cd ${TMPDIR:-/tmp}
+# Find the base Ubuntu version
+#UBUNTU_VERSION=$(inxi -Sx | grep -iPo 'ubuntu [0-9]{2}\.[0-9]{2}' | tr -d '[:alpha:][:blank:]')
+
+# Ask for sudo privilege
+sudo -v || exit $?
+
+# keep-alive sudo session
+while true; do sleep 60; kill -0 "$$" || exit; sudo -v; done 2>/dev/null &
+
+
+
+to_remove=(\
+cachyos-fish-config fish fisher fish-pure-prompt fish-autorepair \
+eza expac shelly meld \
+kdeconnect kcontacts kpeople kwalletmanager\
+)
+
+printf "%s\n" "${to_remove[@]}" > cachyos-remove.lst
+
+sudo pacman -Runs --noconfirm $(pacman -Qq | rg -F -f cachyos-remove.lst)
