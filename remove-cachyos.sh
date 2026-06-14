3e34ebac0f11bd09a07205a8d31fd139295fbed7
duong <dotungduong2@gmail.com>
2026-06-14 11:01:59 +0700

    cachyos: remove plymouth

--- remove-cachyos.sh
+++ remove-cachyos.sh
@@ -16,7 +16,8 @@ while true; do sleep 60; kill -0 "$$" || exit; sudo -v; done 2>/dev/null &
 to_remove=(\
 cachyos-fish-config fish fisher fish-pure-prompt fish-autorepair \
 eza expac shelly meld \
-kdeconnect kcontacts kpeople kwalletmanager\
+kdeconnect kcontacts kpeople kwalletmanager \
+cachyos-plymouth-bootanimation cachyos-plymouth-theme plymouth\
 )
 
 printf "%s\n" "${to_remove[@]}" > cachyos-remove.lst
